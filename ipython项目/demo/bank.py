680ddcbf1b033943d51f4d2e9cb576a4a061293d
Hugh_Y_XIONG <xiongyuanadd@163.com>
2020-10-31 03:28:57 +0800

    master

---
+++ ipython项目/demo/bank.py
@@ -0,0 +1,17 @@
+class bank:
+    def __init__(self,name,age,sex):
+        self.name=name
+        self.age=age
+        self.sex=sex
+    
+    def eat(self):
+        print(self.name,"想吃宵夜")
+    
+    def run(self):
+        print(self.name,"今年",self.age,"岁","性别为：",self.sex)
+
+    def code(self,num):
+        num=1 
+        print(self.name,"计算结果为：",num)
+
+
---
+++ java项目/demo/bank.java
@@ -0,0 +1,6 @@
+class bank{
+    public static void main(String[] args) {
+        System.out.println("真是服了你了");
+    }
+}
+
