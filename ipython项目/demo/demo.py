956b110e53ecc570b451c88e5959195cc080ea2c
Hugh_Y_XIONG <xiongyuanadd@163.com>
2021-02-26 09:42:44 +0800

    first commit

--- ipython项目/demo/demo.py
+++ ipython项目/demo/demo.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import numpy as np
 
+sa ='1'
+saa ='2'
+sss ='3'
+
+cont = []
+cont.append(sa)
+cont.append(saa)
+cont.append(sss)
+
 if __name__ == '__main__':
-    s = 1
-    for i in range(1,30):
-        s = s*2
-        print(s)
+    print(cont)
